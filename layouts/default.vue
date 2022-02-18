1c8ba6eb622612a5a095d8f5ff90f3477741e4a1
Rogério Bayer <rogerio.bayer@ticto.com.br>
2022-02-18 20:53:50 -0300

    [ADD] Commit inicial

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main style="width: 100%;">
+        <Nuxt />
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {  
+      fixed: false,
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="css-selector d-flex">
+      <div class="d-flex align-center justify-center ">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style scoped>
+.css-selector {
+  width: 100%;
+  height:100%;
+  margin:0px;
+  padding:0px;
+    background: linear-gradient(270deg, #2bc29b, #ab2b92, #b1c022);
+    background-size: 600% 600%;
+
+    -webkit-animation: DayGradient 30s ease infinite;
+    -moz-animation: DayGradient 30s ease infinite;
+    animation: DayGradient 30s ease infinite;
+}
+
+@-webkit-keyframes DayGradient {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@-moz-keyframes DayGradient {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@keyframes DayGradient {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+</style>
