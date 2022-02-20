e6d843115fabead773484be468883c6cce54888c
Rogério Bayer <rogerio.bayer@ticto.com.br>
2022-02-20 00:52:49 -0300

    feat: :sparkles: Primeira versão de "hoje é domingo"

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,18 @@
 <template>
   <v-app>
-    <v-main style="width: 100%;">
-        <Nuxt />
+    <v-main style="width: 100%">
+      <Nuxt />
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :absolute="!fixed" style="background: rgba(255, 255, 255, 0.1)">
+      <a
+        href="https://www.linkedin.com/in/rog%C3%A9rio-bayer/"
+        class="ma-0 pa-0"
+      >
+        <img
+          src="rogeriobayer.svg"
+          alt="Rogerio Bayer Logo"
+          class="footer-image ml-3 mt-1"
+      /></a>
     </v-footer>
   </v-app>
 </template>
@@ -13,9 +21,14 @@
 export default {
   name: 'DefaultLayout',
   data() {
-    return {  
+    return {
       fixed: false,
     }
   },
 }
 </script>
+<style scoped>
+.footer-image {
+  height: 17px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,132 @@
 <template>
-<div class="css-selector d-flex">
-      <div class="d-flex align-center justify-center ">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </div>
+  <div
+    class="gradient-container d-flex justify-center"
+    :class="getGradientClass"
+  >
+    <div class="d-flex align-center">
+      <span class="font-weight-light text-h3 text-md-h2 mx-4 ml-md-0">
+        {{ todayIsDomingo }}</span
+      >
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'HomePage',
+  data() {
+    return {}
+  },
+  computed: {
+    todayIsDomingo() {
+      if (new Date().getDay() === 0) {
+        return 'Hoje é domingo! Aproveite!'
+      }
+      return 'Hoje não é domingo'
+    },
+    getGradientClass() {
+      const hour = new Date().getHours()
+      if (hour > 6 && hour < 19) {
+        return 'day-gradient'
+      }
+      return 'night-gradient'
+    },
+  },
+  methods: {},
+  head() {
+    return {
+      title: 'Hoje é domingo?',
+      meta: [
+        {
+          hid: 'Verificador se o dia de hoje é um domingo',
+          name: 'Hoje é domingo?',
+          content: 'Verificador se o dia de hoje é um domingo',
+        },
+      ],
+    }
+  },
 }
 </script>
+
 <style scoped>
-.css-selector {
+.no-select {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.day-gradient {
+  background: linear-gradient(
+    296deg,
+    #03d8d7,
+    #6beba5,
+    #f3db33,
+    #fdd21f,
+    #fda460,
+    #cc3191
+  );
+}
+
+.night-gradient {
+  background: linear-gradient(
+    270deg,
+    #221c64,
+    #44137b,
+    #612797,
+    #3d269b,
+    #0867a8,
+    #a91e9b,
+    #e3478f
+  );
+}
+
+.gradient-container {
   width: 100%;
-  height:100%;
-  margin:0px;
-  padding:0px;
-    background: linear-gradient(270deg, #2bc29b, #ab2b92, #b1c022);
-    background-size: 600% 600%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
 
-    -webkit-animation: DayGradient 30s ease infinite;
-    -moz-animation: DayGradient 30s ease infinite;
-    animation: DayGradient 30s ease infinite;
+  background-size: 1400% 1400%;
+
+  -webkit-animation: GradientAnimation 34s ease infinite;
+  -moz-animation: GradientAnimation 34s ease infinite;
+  animation: GradientAnimation 34s ease infinite;
 }
 
-@-webkit-keyframes DayGradient {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+@-webkit-keyframes GradientAnimation {
+  0% {
+    background-position: 93% 0%;
+  }
+  50% {
+    background-position: 0% 100%;
+  }
+  100% {
+    background-position: 93% 0%;
+  }
 }
-@-moz-keyframes DayGradient {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+@-moz-keyframes GradientAnimation {
+  0% {
+    background-position: 93% 0%;
+  }
+  50% {
+    background-position: 0% 100%;
+  }
+  100% {
+    background-position: 93% 0%;
+  }
 }
-@keyframes DayGradient {
-    0%{background-position:0% 50%}
-    50%{background-position:100% 50%}
-    100%{background-position:0% 50%}
+@keyframes GradientAnimation {
+  0% {
+    background-position: 93% 0%;
+  }
+  50% {
+    background-position: 0% 100%;
+  }
+  100% {
+    background-position: 93% 0%;
+  }
 }
 </style>
