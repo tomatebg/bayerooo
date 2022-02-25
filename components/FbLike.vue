bbfcf25b75a21505afacd233919c448d70519df8
Rogério Bayer <rogerio.bayer@ticto.com.br>
2022-02-24 21:02:15 -0300

    feat: :tada: Integração de botão de like com Facebook

---
+++ components/FbLike.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="fb-root"></div>
+    <script
+      async
+      defer
+      crossorigin="anonymous"
+      src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v13.0"
+      nonce="sX8gWHpg"
+    ></script>
+    <div
+      class="fb-like"
+      data-href="http://bayer.ooo"
+      data-width=""
+      data-layout="button"
+      data-action="like"
+      data-size="small"
+      data-share="false"
+    ></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FbFlke',
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,22 +4,29 @@
       <Nuxt />
     </v-main>
     <v-footer :absolute="!fixed" style="background: rgba(255, 255, 255, 0.1)">
-      <a
-        href="https://www.linkedin.com/in/rog%C3%A9rio-bayer/"
-        class="ma-0 pa-0"
-      >
-        <img
-          src="rogeriobayer.svg"
-          alt="Rogerio Bayer Logo"
-          class="footer-image ml-3 mt-1"
-      /></a>
+      <div class="d-flex justify-start">
+        <a
+          href="https://www.linkedin.com/in/rog%C3%A9rio-bayer/"
+          class="ma-0 pa-0"
+        >
+          <img
+            src="rogeriobayer.svg"
+            alt="Rogerio Bayer Logo"
+            class="footer-image ml-3 mt-1"
+        /></a>
+      </div>
+      <div class="d-flex justify-end">
+        <fb-like />
+      </div>
     </v-footer>
   </v-app>
 </template>
-
 <script>
+import FbLike from '../components/FbLike.vue'
+
 export default {
   name: 'DefaultLayout',
+  components: [FbLike],
   data() {
     return {
       fixed: false,
