bbc52a64083843b2c33489d7424abd9eace90b80
Rogério Bayer <48761175+tomatebg@users.noreply.github.com>
2022-05-21 16:34:35 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  ssr: false,
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
